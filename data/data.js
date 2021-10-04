d58aa13f664415393f44534704cf7ccf9c90f48e
Umratul <umratul0904@gmail.com>
2021-10-04 18:22:27 +0600

    first commit

---
+++ data/data.js
@@ -0,0 +1,114 @@
+const details = [
+    {
+        id:01,
+        name: 'Tradional Indian Cuisine',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Hyderabadi_Chicken_Biryani.jpg',
+        price: 200 ,
+        duration: '5 days',
+        rating: 4.8 ,
+        body:'',
+        lessons: 15
+    },
+    {
+        id:02,
+        name: 'Healthy Smoothies',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/e/eb/Strawberry_and_lemon_smoothie_%2814430283996%29.jpg',
+        price: 150,
+        duration: '3 days' ,
+        rating: 5,
+        body:'',
+        lessons: 7
+    },
+ 
+    {
+        id:03,
+        name: 'Italian Pasta Making',
+        img: 'https://elavegan.com/wp-content/uploads/2019/07/Close-up-of-vegan-baked-ziti-with-mushrooms-and-cheese.jpg',
+        price: 120,
+        duration: '4 days' ,
+        rating: 4,
+        body:'',
+        lessons: 8
+    },
+    {
+        id:04,
+        name: 'Original Ice Creams',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Ice_Cream_dessert_02.jpg/270px-Ice_Cream_dessert_02.jpg',
+        price: 200,
+        duration: '5 days',
+        rating: 4.5,
+        body:'',
+        lessons:12
+    },
+    {
+        id:05,
+        name: 'Sweet Choco Cookies',
+        img: 'https://www.hummingbirdhigh.com/wp-content/uploads/2020/08/01_neapolitan-cookies_IMG_0023.jpg',
+        price: 120 ,
+        duration: '2days' ,
+        rating: 4,
+        body: '' ,
+        lessons: 5
+    },
+    {
+        id:06,
+        name: 'Chocolate Doughnuts',
+        img: 'https://i.ytimg.com/vi/dgn4GScwwx8/maxresdefault.jpg' ,
+        price: 100,
+        duration: '3 days' ,
+        rating: 4.2,
+        body:'',
+        lessons: 5
+    },
+    {
+        id:07,
+        name: 'Simple Delicious Spaghetti',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Pasta_Puttanesca.jpg',
+        price: 90,
+        duration: '2 days',
+        rating: 4,
+        body:'',
+        lessons:4
+    },
+    {
+        id:08,
+        name: 'Coffee Espresso',
+        img: 'https://www.verdict.co.uk/wp-content/uploads/2018/07/shutterstock_203392858.jpg',
+        price: 100,
+        duration: '2 days',
+        rating: 4.5,
+        body:'',
+        lessons:3
+    },
+    {
+        id:09,
+        name: 'Delicious Cakes',
+        img: 'https://food-images.files.bbci.co.uk/food/recipes/rainbow_cake_20402_16x9.jpg',
+        price: 250,
+        duration: '10 days',
+        rating: 5,
+        body: '',
+        lessons:15
+    },
+    {
+        id:10,
+        name: 'Classic Chicken Burgers',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/b/b8/WHOPPER_with_Cheese%2C_at_Burger_King_%282014.05.04%29.jpg',
+        price: 120,
+        duration: '3 days' ,
+        rating: 4.5,
+        body:'',
+        lessons:8
+    },
+    {
+        id:11,
+        name: 'Healthy Fruit Pancakes',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Platt_brunch.jpg/1200px-Platt_brunch.jpg',
+        price: 100,
+        duration:'3 days' ,
+        rating: 4 ,
+        body:'',
+        lessons:5
+    }
+  
+]
